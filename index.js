981382f78f3ca329381e5db88615c2c0880c7d98
axel tavares <tavaresaxel95@gmail.com>
2024-03-01 22:23:27 -0500

    created prompts and creating prompt conditionals

--- index.js
+++ index.js
@@ -7,7 +7,35 @@ const Square = require("./lib/square")
 
 const questions = [
     {
-        type:""
+        type:"list",
+        name:"shape",
+        choices:["circle","triangle","square"]
+    },
+    {
+        type:"input",
+        message:"Enter 3 letters for logo design: ",
+        name:"text"
+    },
+    {
+        type:"input",
+        message:"Plese enter desired color for the background of the shape: ",
+        name:"shapeColor"
+    },
+    {
+        type:"input",
+        message:"Please enter desired color of text",
+        name:"textColor"
     }
 ]
 inquirer.prompt(questions)
+.then(data=>{
+    if(data.shape === "circle"){
+
+    }
+    else if(data === "triangle"){
+        const triangle
+    }
+    else{
+
+    }
+})
